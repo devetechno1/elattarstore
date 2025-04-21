422abd805ca386a72c41c44efd6efabd8c6eccfd
Mohammed Riyad <riyadm2001@gmail.com>
2025-04-21 17:42:07 +0200

    change app name

--- lib/utill/app_constants.dart
+++ lib/utill/app_constants.dart
@@ -2,7 +2,7 @@ import 'package:flutter_sixvalley_ecommerce/localization/models/language_model.d
 import 'package:flutter_sixvalley_ecommerce/utill/images.dart';
 
 class AppConstants {
-  static const String appName = 'El Attar Store';
+  static const String appName = 'El Attar Street';
   static const String slogan = 'E-Commerce Marketplace';
   static const String appVersion = '14.8';
 
